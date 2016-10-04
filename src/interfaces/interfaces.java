5d23ab59cfbcf6db5965f6541de909ca661bd689
blackwidow <xcambil@gmail.com>
2016-10-04 17:18:06 +0200

    FigurasGeometricas

---
+++ src/interfaces/interfaces.java
@@ -0,0 +1,51 @@
+package interfaces;
+
+
+
+/**
+ * Created by blackwidow on 4/10/16.
+ */
+public class interfaces {
+}
+
+class Persona{
+
+}
+
+interface Traballador {
+    public void pagaAlTreballador(Traballador treballador);
+}
+
+class Alumne extends Persona{
+
+}
+
+class Docent extends Persona implements Traballador{
+
+
+    @Override
+    public void pagaAlTreballador(Traballador treballador) {
+
+    }
+}
+
+class Estudiant extends Alumne{
+
+}
+
+class Becari extends Alumne implements Traballador{
+
+    @Override
+    public void pagaAlTreballador(Traballador treballador) {
+
+    }
+}
+
+class Primaria extends Docent{
+
+}
+
+class Secundaria extends Docent{
+
+}
+
